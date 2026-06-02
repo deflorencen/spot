dd9e36f43357624e5598b7cc594dcd2a4c18d32b
yevge <yevgeniysarancha@gmail.com>
2026-06-02 11:41:34 +0200

    test(e2e): add search and verify album page flow

--- tests/e2e/test_search_and_open_artist.py
+++ tests/e2e/test_search_and_open_artist.py
@@ -1,8 +1,14 @@
-from time import sleep
 
 
 def test_search_and_open_artist(app):
     app.home_page.open()
-    app.search.type_query("Ed Sheeran")
-    sleep(10)
 
+    app.search.type_query("Kendrick Lamar")
+
+    app.content.click_artist("Kendrick Lamar")
+    app.content.verify_album_list_visible("DAMN.")
+
+    app.content.click_album("DAMN.")
+
+    app.content.verify_album_page_loaded("DAMN.")
+    app.content.verify_tracks_are_visible(14)
