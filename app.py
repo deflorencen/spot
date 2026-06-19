b7123f63ee4eaaa77971763795be2feb2d51af28
yevge <yevgeniysarancha@gmail.com>
2026-06-19 15:28:35 +0200

    refactor: clean up project repository and remove redundant files

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from playwright.sync_api import Page
 
 from pages.home_page import HomePage
-from pages.auth.login_page import LoginPage
-from pages.auth.signup_page import SignUpPage
 
 from components.search import SearchComponent
 from components.content import ContentComponent
@@ -19,11 +17,3 @@ class Application:
         # LOW PRIORITY
         # self.sidebar = SidebarComponent(page)
 
-        # NOT FOR TESTING
-        self.auth = AuthGroup(page)
-
-
-class AuthGroup:
-    def __init__(self, page: Page):
-        self.login_page = LoginPage(page)
-        self.sign_up_page = SignUpPage(page)
--- pages/home_page.py
+++ pages/home_page.py
@@ -1,11 +1,5 @@
 from data.urls import Urls
-from data.locators.auth_locator import AuthLocators
 from pages.base_page import BasePage
 
 class HomePage(BasePage):
     URL = Urls.HOME_PAGE
-
-    def __init__(self, page):
-        super().__init__(page)
-        self.login_button = page.locator(AuthLocators.LOGIN_BUTTON)
-        self.sign_up_button = page.locator(AuthLocators.SIGN_UP_BUTTON)
